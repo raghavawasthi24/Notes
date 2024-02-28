b2eab25359329c68d1b7feffbae634609dc89800
Raghav Awasthi <raghavawasthi240@gmail.com>
2024-02-28 08:53:21 +0530

    refactored dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LeftSideBar from "../components/LeftSideBar";
 import InputBar from "../components/InputBar";
 import Notes from "../components/Notes";
 
 export default function Dashboard() {
+  useEffect(()=>{
+
+  },[])
   return (
     <div className="bg-gray-200 w-screen h-screen overflow-hidden flex">
       <LeftSideBar />
